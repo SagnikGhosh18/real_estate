6894c44ee5a66970443c80bcfe9fd1ee95d7b503
SagnikGhosh18 <sagnikghosh18@gmail.com>
2025-02-09 17:23:21 +0530

    Aetup with auth

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,10 +10,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold my-10 text-3xl font-rubik">Welcome</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Propery</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
